e5f0003c02d191d62cf5aa84716e205654437d27
游天琪 <autumnyout@gmail.com>
2020-12-10 15:53:37 +0800

    output

---
+++ packages/output-management/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'click me and check console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+  return element;
+}
+document.body.appendChild(component());
