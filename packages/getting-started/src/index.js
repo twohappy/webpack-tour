3a4fdad3a8eabab69d8f6e4d3b1bb1648a8f42d8
游天琪 <autumnyout@gmail.com>
2020-12-11 14:19:51 +0800

    test

--- packages/getting-started/src/index.js
+++ packages/getting-started/src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 function component() {
+  console.log('???')
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -8,4 +9,8 @@ function component() {
     return element;
   }
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
+
+export function hello(){
+  console.log('helo from getting start')
+}
